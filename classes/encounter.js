47d1ab3387050af8f0bc7f7902c7d801bf4b83a7
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-12 00:10:37 -0400

    WIP

--- classes/encounter.js
+++ classes/encounter.js
@@ -11,11 +11,18 @@ Encounter.prototype = new Card();
 Encounter.prototype.flip
 Encounter.prototype.place = function(){
   var encounter = this;
+  var choices   = h.el("[data-choices]>*", encounter.el);
+  h.for_each(choices, function(choice){
+    choice.addEventListener("click", function(){
+      var el = this;
+      var isRoll  = el.hasAttribute("data-roll");
+      var choice  = h.el("[data-choice]", el).getAttribute("data-choice");
+    });
+  });
   document.body.appendChild(encounter.el);
 }
 Encounter.prototype.remove
 Encounter.prototype.getConquered
-Encounter.prototype.choices = []
 
 function Monster(){}
 Monster.prototype = new Encounter();
