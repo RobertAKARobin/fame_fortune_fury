ec6fe4262acd273798c1638a5b7d0fd2d0098378
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-18 18:58:43 -0500

    Incorporated h#chain

--- classes/card.js
+++ classes/card.js
@@ -6,37 +6,43 @@ function Deck(klass){
 }
 Deck.prototype.load = function(cardsLoaded){
   var deck = this;
-  h.ajax("./templates/" + deck.classname + ".html", loadTemplate);
-  function loadTemplate(html){
-    deck.class.template = html.split(/\{\{.*?\}\}/);
-    deck.class.fields = [];
-    html.replace(/\{\{(.*?)\}\}/g, function(a, field){
-      deck.class.fields.push(field);
-    });
-    templateLoaded();
-  }
-  function templateLoaded(){
-    h.ajax("./cards/" + deck.classname + "s/_index.json", loadCards);
-  }
-  function loadCards(cardList){
-    h.for_each(cardList, function(listItem, abbr, next){
-      h.ajax("./cards/" + deck.classname + "s/" + abbr + ".html", function(html){
-        var card    = new deck.class();
-        card.class  = deck.class;
-        card.deck   = deck;
-        card.html   = html;
-        card.abbr   = abbr;
-        deck.cards[abbr] = card;
-        h.extend(card, listItem);
-        if(card.title === undefined){
-          card.title = h.collect(card.abbr.split("_"), function(word){
-            return h.capitalize(word);
-          }).join(" ");
-        }
-        next();
+  h.chain([
+    function(next){
+      h.ajax("./templates/" + deck.classname + ".html", next);
+    },
+    function(next, data){
+      var html = data[0];
+      deck.class.template = html.split(/\{\{.*?\}\}/);
+      deck.class.fields = [];
+      html.replace(/\{\{(.*?)\}\}/g, function(a, field){
+        deck.class.fields.push(field);
       });
-    }, cardsLoaded);
-  }
+      next();
+    },
+    function(next){
+      h.ajax("./cards/" + deck.classname + "s/_index.json", next);
+    },
+    function(next, data){
+      var cardList = data[0];
+      h.for_each(cardList, function(listItem, abbr, next){
+        h.ajax("./cards/" + deck.classname + "s/" + abbr + ".html", function(html){
+          var card    = new deck.class();
+          card.class  = deck.class;
+          card.deck   = deck;
+          card.html   = html;
+          card.abbr   = abbr;
+          deck.cards[abbr] = card;
+          h.extend(card, listItem);
+          if(card.title === undefined){
+            card.title = h.collect(card.abbr.split("_"), function(word){
+              return h.capitalize(word);
+            }).join(" ");
+          }
+          next();
+        });
+      }, cardsLoaded);
+    }
+  ]);
 }
 Deck.prototype.shuffle;
 Deck.prototype.draw;
